ce9c55192275a22d7ff7aad36681416f2e431d75
Ka Hea Lam <kahealam@gmail.com>
2020-12-16 22:19:59 +0100

    About pagina toegevoegd

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('content')
+    <h1 class="bronnen">Gebruikte bronnen</h1>
+    <p><b>Database verbinden:</b> <a href="https://www.youtube.com/watch?v=LNeS6BPLlus&t=756s&ab_channel=Coder%27sTape">Link</a></p>
+    <p><b>Seeder aanmaken voor DB:</b> <a href="https://stackoverflow.com/questions/34382043/laravel-how-do-i-insert-the-first-user-in-the-database">Link</a> <a href="https://stackoverflow.com/questions/64406855/laravel-8-class-database-seeders-db-not-found">Link</a></p>
+    <p><b>Authentication - Register, Login & password Reset:</b> <a href="https://www.youtube.com/watch?v=BvwEaWfpXY0&t=282s&ab_channel=Coder%27sTape">Link</a></p>
+    <p><b></b><a href=""></a></p>
+@endsection
